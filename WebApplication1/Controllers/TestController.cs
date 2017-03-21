3be8c8ab4a7fc6b1058379db2b165fabf31f7908
Rob <riedinger_robbie@yahoo.com>
2017-03-21 09:31:54 -0500

    uikhjkj

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        private Models.Person p = new Models.Person();
+        // GET: Default
+        public ActionResult Index()
+        {
+            p.FirstName = "Rob";
+            p.LastName = "Riedinger";
+            p.TwitterHandle = "@riedstep";
+            return View(p);
+        }
+    }
+}
